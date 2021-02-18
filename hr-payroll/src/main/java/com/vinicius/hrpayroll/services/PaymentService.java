9c5f72f9ae6608ce34b04f6ce60c840f42041beb
Avinicius36 <avinicius36@gmail.com>
2021-02-17 22:38:37 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/vinicius/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/vinicius/hrpayroll/services/PaymentService.java
@@ -4,12 +4,8 @@ import com.vinicius.hrpayroll.entities.Payment;
 import com.vinicius.hrpayroll.entities.Worker;
 import com.vinicius.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {

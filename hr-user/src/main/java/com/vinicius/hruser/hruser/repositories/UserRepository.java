dfb4d691fa38d1c1a523ec2e594ae131d013e7fe
Avinicius36 <avinicius36@gmail.com>
2021-03-10 13:52:16 -0300

    Configurando o serviço de User

---
+++ hr-user/src/main/java/com/vinicius/hruser/hruser/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.vinicius.hruser.hruser.repositories;
+
+import com.vinicius.hruser.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}

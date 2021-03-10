4777694a4942c5d676512a043d3264b626b27ef7
Avinicius36 <avinicius36@gmail.com>
2021-03-10 10:45:40 -0300

    Creating hr-user

---
+++ hr-user/src/test/java/com/vinicius/hruser/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.hruser.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
